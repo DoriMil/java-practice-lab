4d79489ee497c48b24fe81114352effbd5452827
Dorota Miler <dorota.miler@capgemini.com>
2025-05-19 14:44:23 +0200

    Refactor WhoLikesIt and enhance test coverage
    * Renamed package structure for clarity and added multiple solutions to the `WhoLikesIt` logic
    * Enhanced tests to cover all solutions using JUnit 5, replacing JUnit 4 dependencies in the build configuration for better modern testing support

--- src/main/java/com/dorotamiler/Main.java
+++ src/main/java/com/dorotamiler/Main.java
@@ -1,9 +1,11 @@
 package com.dorotamiler;
 
-import com.dorotamiler.codewars.kyu6.WhoLikesIt;
+import com.dorotamiler.codewars.kyu6.wholikesit.WhoLikesIt;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(WhoLikesIt.whoLikesIt("Peter"));
+        System.out.println(WhoLikesIt.solution1("Peter"));
+        System.out.println(WhoLikesIt.solution2("Peter"));
+        System.out.println(WhoLikesIt.solution3("Peter"));
     }
 }
---
+++ src/main/java/com/dorotamiler/codewars/kyu6/wholikesit/WhoLikesIt.java
@@ -0,0 +1,46 @@
+package com.dorotamiler.codewars.kyu6.wholikesit;
+
+public class WhoLikesIt {
+
+    /**
+     * These methods method takes a variable number of names and returns a string indicating who likes something.
+     * The output varies based on the number of names provided.
+     *
+     * @param names A variable number of names (String).
+     * @return A string indicating who likes something.
+     */
+
+    public static String solution1(String... names) {
+        if (names.length == 0) {
+            return "no one likes this";
+        } else if (names.length == 1) {
+            return names[0] + " likes this";
+        } else if (names.length == 2) {
+            return names[0] + " and " + names[1] + " like this";
+        } else if (names.length == 3) {
+            return names[0] + ", " + names[1] + " and " + names[2] + " like this";
+        } else {
+            return names[0] + ", " + names[1] + " and " + (names.length - 2) + " others like this";
+        }
+    }
+
+    public static String solution2(String... names) {
+        return switch (names.length) {
+            case 0 -> "no one likes this";
+            case 1 -> names[0] + " likes this";
+            case 2 -> names[0] + " and " + names[1] + " like this";
+            case 3 -> names[0] + ", " + names[1] + " and " + names[2] + " like this";
+            default -> names[0] + ", " + names[1] + " and " + (names.length - 2) + " others like this";
+        };
+    }
+
+    public static String solution3(String... names) {
+        return switch (names.length) {
+            case 0 -> "no one likes this";
+            case 1 -> "%s likes this".formatted(names[0]);
+            case 2 ->  "%s and %s like this".formatted(names[0],names[1]);
+            case 3 ->  "%s, %s and %s like this".formatted(names[0], names[1], names[2]);
+            default ->  "%s, %s and %d others like this".formatted(names[0],names[1], names.length - 2);
+        };
+    }
+}
